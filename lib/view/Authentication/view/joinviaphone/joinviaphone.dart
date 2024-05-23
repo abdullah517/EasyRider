cf713d46099e82c0bb1b17bbfa733671b2af6a73
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-05-23 18:45:12 +0500

    Some Updates

--- lib/view/Authentication/view/joinviaphone/joinviaphone.dart
+++ lib/view/Authentication/view/joinviaphone/joinviaphone.dart
@@ -52,9 +52,8 @@ class Joinviaphone extends StatelessWidget {
                   Consumer<Joinviaphoneprovider>(
                     builder: (context, checkstate, child) => Checkbox(
                       value: checkstate.ischecked,
-                      fillColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) =>
-                              Appcolors.successColor),
+                      fillColor: WidgetStateProperty.resolveWith<Color>(
+                          (Set<WidgetState> states) => Appcolors.successColor),
                       onChanged: (value) {
                         checkstate.setcheckstate(value);
                       },

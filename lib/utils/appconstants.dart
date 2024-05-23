4e9bbd7653431df7387da88e4510209124ab30c3
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-05-23 14:58:32 +0500

    Some Updates

--- lib/utils/appconstants.dart
+++ lib/utils/appconstants.dart
@@ -1,6 +1,6 @@
 class Appconstants {
   static const String onboardtxt =
-      'Sell houses easily with the help of\nListenoryx and to make this line big\nI am writing more.';
+      'Welcome to RideeShare! Find rides quickly and\nsafely with our reliable service.\nJoin us and start your journey today.';
   static const String congrattext =
       'Our team will review your application. You will be\nnotified when your application is approved with in\n24 hours.';
 }

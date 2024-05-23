f300b2813f5011592eb3013e95bf5ff31c7b978a
Muhammad Abdur-Rehman <muhammadabdurrehman516@gmail.com>
2024-05-23 19:21:11 +0500

    Some Updates

--- lib/view/Homepage/components/sidemenubar.dart
+++ lib/view/Homepage/components/sidemenubar.dart
@@ -4,8 +4,13 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:ridemate/Providers/userdataprovider.dart';
 import 'package:ridemate/routing/routing.dart';
+import 'package:ridemate/sidemenupages/aboutus.dart';
+import 'package:ridemate/sidemenupages/deleteaccount.dart';
+import 'package:ridemate/sidemenupages/helpandsupport.dart';
+import 'package:ridemate/sidemenupages/privacy.dart';
 import 'package:ridemate/utils/appcolors.dart';
 import 'package:ridemate/view/Authentication/view/Driver/driverscreen.dart';
+import 'package:ridemate/view/Authentication/view/Driver/goingtoworkas.dart';
 import 'package:ridemate/view/Homepage/components/menubarcomp.dart';
 import 'package:ridemate/view/Welcomescreen/welcomescreen.dart';
 import 'package:ridemate/widgets/custombutton.dart';
@@ -64,18 +69,42 @@ class Sidemenubar extends StatelessWidget {
                           as ImageProvider
                       : NetworkImage(usermap.userData['Profileimage']),
                 )),
-            const Menubarcomp(text: 'Edit Profile', icon: Icons.person_outline),
-            const Divider(color: Appcolors.neutralgrey, height: 1),
-            const Menubarcomp(text: 'History', icon: Icons.history),
-            const Divider(color: Appcolors.neutralgrey, height: 1),
-            const Menubarcomp(text: 'Complain', icon: Icons.info),
-            const Divider(color: Appcolors.neutralgrey, height: 1),
-            const Menubarcomp(text: 'About Us', icon: Icons.info_outline),
-            const Divider(color: Appcolors.neutralgrey, height: 1),
-            const Menubarcomp(text: 'Settings', icon: Icons.settings),
-            const Divider(color: Appcolors.neutralgrey, height: 1),
             const Menubarcomp(
-                text: 'Help and Support', icon: Icons.help_outline_outlined),
+              text: 'History',
+              icon: Icons.history,
+            ),
+            const Divider(color: Appcolors.neutralgrey, height: 1),
+            Menubarcomp(
+              text: 'About Us',
+              icon: Icons.info_outline,
+              onTap: () {
+                navigateToScreen(context, const AboutUsPage());
+              },
+            ),
+            const Divider(color: Appcolors.neutralgrey, height: 1),
+            Menubarcomp(
+              text: 'Privacy & Policy',
+              icon: Icons.privacy_tip,
+              onTap: () {
+                navigateToScreen(context, const PrivacyPolicyPage());
+              },
+            ),
+            const Divider(color: Appcolors.neutralgrey, height: 1),
+            Menubarcomp(
+              text: 'Delete Account',
+              icon: Icons.account_box,
+              onTap: () {
+                navigateToScreen(context, const DeleteAccountPage());
+              },
+            ),
+            const Divider(color: Appcolors.neutralgrey, height: 1),
+            Menubarcomp(
+              text: 'Help and Support',
+              icon: Icons.help_outline_outlined,
+              onTap: () {
+                navigateToScreen(context, const HelpSupportPage());
+              },
+            ),
             const Divider(color: Appcolors.neutralgrey, height: 1),
             Menubarcomp(
                 text: 'Logout', icon: Icons.logout_outlined, onTap: logout),
